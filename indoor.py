8e306919a199555aeb651704a8338063743580f5
umamimelly <110947707+umamimelly@users.noreply.github.com>
2022-08-17 16:43:49 -0600

    Create indoor.py

---
+++ indoor.py
@@ -0,0 +1,16 @@
+#hello = "HELLO"
+#print(hello.lower())
+
+#whatclass = "THIS IS CS50"
+#print(whatclass.lower())
+
+#fiddy = "50"
+#print(fiddy)
+
+#userinp=input()
+#print(userinp.lower())
+
+#let's make this better now that the puzzle pieces are coming together in my noodle
+
+userinp = input()
+print(userinp.lower())
