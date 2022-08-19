29e1daa087aaa53d0e311e37cf978ab3e49d2eb9
umamimelly <110947707+umamimelly@users.noreply.github.com>
2022-08-19 15:26:28 -0600

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,11 +1,11 @@
 #write the question asking for the answer of the life, universe, and everything
-
 whatis = input("What is the answer to life, the universe, and everything? ")
 
 if whatis.casefold() == "forty two":
     print("Yes")
 elif whatis.casefold() == "forty-two":
     print("Yes")
-elif whatis == "42":
+elif whatis.strip() == "42":
     print("Yes")
 else: print("No")
+
